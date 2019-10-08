82c662454c5e0a50bc4018ee7916e3dc54eb378f
Allison Johnson <allison.cuttler@gmail.com>
2019-10-08 17:03:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(calls)
-  calls.map{|call| ""}
+  calls.map{|call| "#{call.capitalize()}!"}
 end
 
 def long_planeteer_calls# code an argument here
